9e65e7d14d72e1864b5fd6ed372e8167f349f4cf
aras armani <aras.armani2014@gmail.com>
2018-10-07 14:54:15 +0300

    RN Video stream is added

--- App.js
+++ App.js
@@ -1,29 +1,101 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+ 
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+ 
+import Video from 'react-native-video';
+import MediaControls, { PLAYER_STATES } from 'react-native-media-controls';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+class App extends Component {
+  videoPlayer;
 
-type Props = {};
-export default class App extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentTime: 0,
+      duration: 0,
+      isFullScreen: false,
+      isLoading: true,
+      paused: false,
+      playerState: PLAYER_STATES.PLAYING,
+    };
+  }
+
+        onSeek = seek => {
+          this.videoPlayer.seek(seek);
+        };
+
+        onPaused = playerState => {
+          this.setState({
+            paused: !this.state.paused,
+            playerState,
+          });
+        };
+
+        onReplay = () => {
+          this.setState({ playerState: PLAYER_STATES.PLAYING });
+          this.videoPlayer.seek(0);
+        };
+
+        onProgress = data => {
+          const { isLoading, playerState } = this.state;
+          // Video Player will continue progress even if the video already ended
+          if (!isLoading && playerState !== PLAYER_STATES.ENDED) {
+            this.setState({ currentTime: data.currentTime });
+          }
+        };
+
+        onLoad = data => this.setState({ duration: data.duration, isLoading: false });
+
+        onLoadStart = data => this.setState({ isLoading: true });
+
+        onEnd = () => this.setState({ playerState: PLAYER_STATES.ENDED });
+
+        onError = () => alert('Oh! ', error);
+
+        exitFullScreen = () => {};
+
+        enterFullScreen = () => {};
+
+        onFullScreen = () => {};
+
+        renderToolbar = () => (
+          <View style={styles.toolbar}>
+            <Text>I'm a custom toolbar </Text>
+          </View>
+        );
+
+        onSeeking = currentTime => this.setState({ currentTime });
+        
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Video
+          onEnd={this.onEnd}
+          onLoad={this.onLoad}
+          onLoadStart={this.onLoadStart}
+          onProgress={this.onProgress}
+          paused={this.state.paused}
+          ref={videoPlayer => (this.videoPlayer = videoPlayer)}
+          resizeMode="content"
+          source={{ uri: 'https://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4' }}
+          // source={{ uri: 'http://napi.starsapp.ir/files/stars/17837/videos/1008-17837-1530085991.mp4' }}
+          style={styles.mediaPlayer}
+          volume={10}
+        />
+        <MediaControls
+          duration={this.state.duration}
+          isLoading={this.state.isLoading}
+          mainColor="#333"
+          onFullScreen={this.onFullScreen}
+          onPaused={this.onPaused}
+          onReplay={this.onReplay}
+          onSeek={this.onSeek}
+          onSeeking={this.onSeeking}
+          playerState={this.state.playerState}
+          progress={this.state.currentTime}
+          toolbar={this.renderToolbar()}
+        />
       </View>
     );
   }
@@ -32,18 +104,22 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  toolbar: {
+    marginTop: 30,
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 5,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  mediaPlayer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    backgroundColor: 'black',
   },
 });
+
+
+export default App;
